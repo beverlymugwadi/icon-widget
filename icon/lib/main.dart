a6fe5015eaff71f29d7ff2056b77dbed6a1726de
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2025-06-12 21:58:47 +0200

    finalising

--- icon/lib/main.dart
+++ icon/lib/main.dart
@@ -1,7 +1,11 @@
+// Import the Flutter material package
 import 'package:flutter/material.dart';
 
+
+// Entry point of the Flutter application
 void main() => runApp(MyApp());
 
+// Define the main application widget
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   
@@ -10,11 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Icon Widget Demo')),
+        // Center the body content
         body: Center(
+          // Layout children horizontally
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              //iIcons with different colors and sizes
               Icon(Icons.favorite, color: Colors.red, size: 40),
+              // Add spacing between icons
               SizedBox(width: 20),
               Icon(Icons.thumb_up, color: Colors.blue, size: 40),
               SizedBox(width: 20),
